1cb5b9f8c9f46cf1342321d7d42eb1a2cea6aab8
Douglas Silva <douglasbritodev@gmail.com>
2026-08-11 21:58:10 -0300

    primeiroComando

---
+++ LerArquivo.py
@@ -0,0 +1,6 @@
+import os
+
+caminho_da_pasta = r"C:\Users\dougl\Desktop\Trabalho_sql\1-lh_nautical_csv"
+# 1. Pega a lista com TODOS os arquivos da pasta
+todos_os_arquivos = os.listdir(caminho_da_pasta)
+
