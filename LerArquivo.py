bc783dbc92d63b5b9196d1759263b08a52b4e2ba
Douglas Silva <douglasbritodev@gmail.com>
2026-08-11 21:59:44 -0300

    segundoComando

--- LerArquivo.py
+++ LerArquivo.py
@@ -4,3 +4,6 @@ caminho_da_pasta = r"C:\Users\dougl\Desktop\Trabalho_sql\1-lh_nautical_csv"
 # 1. Pega a lista com TODOS os arquivos da pasta
 todos_os_arquivos = os.listdir(caminho_da_pasta)
 
+# 2. O 'for' passa por arquivo por arquivo dessa lista:
+for arquivo in todos_os_arquivos:
+    
